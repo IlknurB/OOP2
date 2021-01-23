8a352258c3165dc0f5ddd0f1ec3757b104e93a20
İlknur Binboğa <nur.binboga@gmail.com>
2021-01-23 23:56:22 +0300

    Add project files.

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 2;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "İlknur";
+            musteri1.Soyadi = "Binboğa";
+            musteri1.TcNo = "12345678910";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "123456";
+            musteri2.SirketAdi = "İnşaat";
+            musteri2.VergiNo = "2546942142";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+
+            
+
+               
+                
+              
+        }
+    }
+}
